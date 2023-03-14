341de25f252f5e6f298f38c005100cde13666b30
LukasMartini <60274604+LukasMartini@users.noreply.github.com>
2023-03-13 21:55:02 -0400

    Fixed issue with mana symbol parsing that resulted in double-digit values being split into each digit separately.

--- manaSymbols.py
+++ manaSymbols.py
@@ -14,16 +14,13 @@ class ManaSymbols(QWidget):
         #       - There is likely to be an issue with mana symbols that I forgot to include/how they format infinity in the json.
         # FIXED - Fix the spacing on the layout with styling, shouldn't be difficult
         # FIXED - Phyrexian symbols do not work as of this moment (they're replaced by the non-phyrexian counterpart)
-
-        currentPositionInString = 0
-        for each in manaCost:
+        for each in manaCost.split('{')[1::]: # Loops through mana cost string split by {, starting from the second item as the first item is '', caused by { being the first character of any cost string.
             newLabel = QLabel()
-            if currentPositionInString + 1 < len(manaCost) and manaCost[currentPositionInString+1] == '/':
-                newLabel.setPixmap(QPixmap(':/resources/' + each + 'P.png'))
+            if '/' in each: # Handles Phyrexian mana
+                newLabel.setPixmap(QPixmap(':/resources/' + each[0] + 'P.png'))
                 self.costLayout.addWidget(newLabel)
-            elif each != '{' and each != '}':
-                newLabel.setPixmap(QPixmap(':/resources/' + each + '.png'))
+            else:
+                newLabel.setPixmap(QPixmap(':/resources/' + each[:len(each)-1:] + '.png')) # each[:len(each)-1:] is just indexing until the second last character in the string (1 before the closing })
                 self.costLayout.addWidget(newLabel)
-            currentPositionInString += 1
 
         self.setLayout(self.costLayout)
